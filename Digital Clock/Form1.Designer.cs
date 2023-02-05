b31e6c459b721d865f208362eee39affca94ebe7
Ryna K <safaryna79@gmail.com>
2023-02-06 00:58:11 +0800

    Add project files.

---
+++ Digital Clock/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace Digital_Clock
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.labelTime = new System.Windows.Forms.Label();
+            this.labelSeconds = new System.Windows.Forms.Label();
+            this.labelDate = new System.Windows.Forms.Label();
+            this.labelDay = new System.Windows.Forms.Label();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // labelTime
+            // 
+            this.labelTime.AutoSize = true;
+            this.labelTime.Font = new System.Drawing.Font("Segoe UI", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTime.Location = new System.Drawing.Point(27, 23);
+            this.labelTime.Name = "labelTime";
+            this.labelTime.Size = new System.Drawing.Size(202, 86);
+            this.labelTime.TabIndex = 0;
+            this.labelTime.Text = "23:23";
+            this.labelTime.TextChanged += new System.EventHandler(this.label2_Click);
+            // 
+            // labelSeconds
+            // 
+            this.labelSeconds.AutoSize = true;
+            this.labelSeconds.Font = new System.Drawing.Font("Segoe UI", 22F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelSeconds.Location = new System.Drawing.Point(207, 60);
+            this.labelSeconds.Name = "labelSeconds";
+            this.labelSeconds.Size = new System.Drawing.Size(50, 41);
+            this.labelSeconds.TabIndex = 1;
+            this.labelSeconds.Text = "23";
+            this.labelSeconds.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // labelDate
+            // 
+            this.labelDate.AutoSize = true;
+            this.labelDate.Font = new System.Drawing.Font("Segoe UI Semibold", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelDate.Location = new System.Drawing.Point(27, 109);
+            this.labelDate.Name = "labelDate";
+            this.labelDate.Size = new System.Drawing.Size(477, 86);
+            this.labelDate.TabIndex = 2;
+            this.labelDate.Text = "January 6 2023";
+            // 
+            // labelDay
+            // 
+            this.labelDay.AutoSize = true;
+            this.labelDay.Font = new System.Drawing.Font("Segoe UI", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelDay.Location = new System.Drawing.Point(31, 186);
+            this.labelDay.Name = "labelDay";
+            this.labelDay.Size = new System.Drawing.Size(201, 65);
+            this.labelDay.TabIndex = 3;
+            this.labelDay.Text = "Monday";
+            this.labelDay.Click += new System.EventHandler(this.timer_Tick);
+            // 
+            // timer
+            // 
+            this.timer.Interval = 1000;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkOliveGreen;
+            this.ClientSize = new System.Drawing.Size(557, 292);
+            this.Controls.Add(this.labelDay);
+            this.Controls.Add(this.labelDate);
+            this.Controls.Add(this.labelSeconds);
+            this.Controls.Add(this.labelTime);
+            this.ForeColor = System.Drawing.Color.White;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelTime;
+        private System.Windows.Forms.Label labelSeconds;
+        private System.Windows.Forms.Label labelDate;
+        private System.Windows.Forms.Label labelDay;
+        private System.Windows.Forms.Timer timer;
+    }
+}
+
